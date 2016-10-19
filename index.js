c619502b2d22077f0a84cd5566e62a053cddece7
Yaodong Li <mail@liyaodong.com>
2016-10-19 10:34:40 +0800

    First blood

---
+++ index.js
@@ -0,0 +1,9 @@
+exports.parseString = str => {
+  let result = [];
+
+  for (let i = 0; i < str.length; i++) {
+    result.push(str[i]);
+  }
+
+  return result;
+};
---
+++ spec/index.spec.js
@@ -0,0 +1,7 @@
+const { parseString } = require('../index');
+
+describe("index", function() {
+  it("return a new array with string length", function() {
+    expect(parseString('abcd')).toEqual(['a', 'b', 'c', 'd']);
+  });
+});
